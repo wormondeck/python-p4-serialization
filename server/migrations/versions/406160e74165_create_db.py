efea8fdca369be5046121455d889f256bef44310
Ben Botsford <ben.botsford@flatironschool.com>
2023-05-02 15:26:04 -0500

    add migrations

---
+++ server/migrations/versions/406160e74165_create_db.py
@@ -0,0 +1,24 @@
+"""create db
+
+Revision ID: 406160e74165
+Revises: 
+Create Date: 2023-05-02 14:13:11.284079
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '406160e74165'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
